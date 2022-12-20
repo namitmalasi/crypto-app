77471ea541ccee6ac72f092bc01c02e85fa59d3e
Namit Malasi <namitmalasi@gmail.com>
2022-12-20 19:27:56 +0530

    setting up folder structure and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Coins from "./pages/Coins";
+import Exchanges from "./pages/Exchanges";
+import CoinDetails from "./pages/CoinDetails";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />{" "}
+        <Route path="/exchanges" element={<Exchanges />} />{" "}
+        <Route path="/coin/:id" element={<CoinDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
