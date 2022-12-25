5a00780fafaeae664da251469f283dc9714c54ef
Namit Malasi <namitmalasi@gmail.com>
2022-12-25 20:21:00 +0530

    minor change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import { Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const Footer = () => {
